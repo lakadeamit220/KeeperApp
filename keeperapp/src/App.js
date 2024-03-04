0c6b01c823bc6456d44f6d5cf689b55ff50d5271
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2024-03-04 11:39:14 +0530

    added few new notes with the help of map function

--- keeperapp/src/App.js
+++ keeperapp/src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 import Header from './components/Header';
 import Note from './components/Note';
 import Footer from './components/Footer';
+import notes from './components/notes';
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {
+        notes.map((n) => (
+          <Note
+            key={n.id}
+            title={n.title}
+            content={n.content}
+          />
+        ))
+      }
       <Footer />
     </div>
   );
