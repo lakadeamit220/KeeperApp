0acf9f2f4a797c8ca5c557a392474b08bc7a4b35
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2024-03-05 16:56:53 +0530

    modified keeperapp

--- keeperapp/src/App.js
+++ keeperapp/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Note from './components/Note';
-import Footer from './components/Footer';
-import notes from './components/notes';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      {
-        notes.map((n) => (
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
           <Note
-            key={n.id}
-            title={n.title}
-            content={n.content}
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
           />
-        ))
-      }
+        );
+      })}
       <Footer />
     </div>
   );
